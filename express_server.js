7d28f77c40362fd472fe202547a95ea7ef53b18b
Ryan <cordingley.ryan@gmail.com>
2019-10-28 23:01:20 +0000

    added new route for short urls

--- express_server.js
+++ express_server.js
@@ -14,6 +14,11 @@ app.get("/", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
