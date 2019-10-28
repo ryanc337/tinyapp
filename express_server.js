d04a0ea15ff7645c0191afc47321c988a106dd20
Ryan <cordingley.ryan@gmail.com>
2019-10-28 23:21:28 +0000

    updated express server file to have correct routes

--- express_server.js
+++ express_server.js
@@ -9,7 +9,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
+app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
